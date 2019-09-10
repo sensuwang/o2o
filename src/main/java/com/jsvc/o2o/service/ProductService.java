d3b519e6f491c704b3bdf40f940df3b6292cdbc1
sensu <wjs11234567@163.com>
2019-09-10 15:06:21 +0800

    ImageHolder重构

--- src/main/java/com/jsvc/o2o/service/ProductService.java
+++ src/main/java/com/jsvc/o2o/service/ProductService.java
@@ -8,24 +8,23 @@ import com.jsvc.o2o.entity.Product;
 import org.springframework.web.multipart.commons.CommonsMultipartFile;
 
 
-
 public interface ProductService {
-	ProductExecution getProductList(Product productCondition, int pageIndex, int pageSize);
+    ProductExecution getProductList(Product productCondition, int pageIndex, int pageSize);
 
-	Product getProductById(long productId);
+    Product getProductById(long productId);
 
-	/**
-	 * 商品信息上传以及图片处理
-	 * @param product 商品信息
-	 * @param thumbnail 缩略图信息
-	 * @param productImgList 详细图片信息
-	 * @return
-	 * @throws RuntimeException
-	 */
-	ProductExecution addProduct(Product product,ImageHolder thumbnail,
-								List<ImageHolder> productImgList)
-			throws RuntimeException;
+    /**
+     * 商品信息上传以及图片处理
+     *
+     * @param product        商品信息
+     * @param thumbnail      缩略图信息
+     * @param productImgList 详细图片信息
+     * @return
+     * @throws RuntimeException
+     */
+    ProductExecution addProduct(Product product, ImageHolder thumbnail,
+                                List<ImageHolder> productImgList) throws RuntimeException;
 
-	ProductExecution modifyProduct(Product product, ImageHolder thumbnail,
+    ProductExecution modifyProduct(Product product, ImageHolder thumbnail,
                                    List<ImageHolder> productImgs) throws RuntimeException;
 }
