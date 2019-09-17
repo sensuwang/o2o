563009a7fddd4583f829d1a3b23f3c1ebeddf3de
sensu <wjs11234567@163.com>
2019-09-17 15:47:37 +0800

    数据库连接串加密

---
+++ src/main/java/com/jsvc/o2o/util/EncryptPropertyPlaceholderConfigurer.java
@@ -0,0 +1,41 @@
+package com.jsvc.o2o.util;
+
+import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
+
+public class EncryptPropertyPlaceholderConfigurer extends
+		PropertyPlaceholderConfigurer {
+	//需要加密的字段数组
+	private String[] encryptPropNames = { "jdbc.username", "jdbc.password" };
+
+	/**
+	 * 对关键的属性进行转换
+	 * @param propertyName
+	 * @param propertyValue
+	 * @return
+	 */
+	@Override
+	protected String convertProperty(String propertyName, String propertyValue) {
+		if (isEncryptProp(propertyName)) {
+			//对已加密的字段进行解密工作
+			String decryptValue = DESUtils.getDecryptString(propertyValue);
+			return decryptValue;
+		} else {
+			return propertyValue;
+		}
+	}
+
+	/**
+	 * 判断该属性是否已经加密
+	 * @param propertyName
+	 * @return
+	 */
+	private boolean isEncryptProp(String propertyName) {
+		//通过上面的string数组进行判断
+		for (String encryptpropertyName : encryptPropNames) {
+			if (encryptpropertyName.equals(propertyName))
+				return true;
+		}
+		return false;
+	}
+
+}
